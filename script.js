7b825c0122706359299fa800da5b6cb5328601f8
Grifanya9 <hryhorii.a.petrenko@gmail.com>
2020-04-21 00:30:31 +0300

    new change

--- script.js
+++ script.js
@@ -1,56 +1,72 @@
-const input = document.querySelector(".todo-input");
-const list = document.querySelector(".todo-list");
+function ToDolist(insertPoint, todos) {
+    const containerHtml = `
+            <div class="todo-container">
+                <h1 class="todo-title">todos</h1>
+                <ul class="todo-list"></ul>
+                <input class="todo-input" type="text" placeholder="E.g. Build a web app">
+            </div>
+        `;
+    insertPoint.insertAdjacentHTML("afterbegin", containerHtml);
 
-const todos = [
-    "todo1",
-    "todo2",
-    "todo3",
-    "todo4",
-];
+    const container = insertPoint.children.item(0);
+    const input = container.querySelector(".todo-input");
+    const list = container.querySelector(".todo-list");
 
-for (let i = 0; i < todos.length; i++) {
-    const todo = todos[i];
-    addToDo(todo);
-}
+    todos.forEach(function (todo) {
+        addToDo(todo);
+    });
 
-function addToDo(toDo) {
-    const item = `
+    function addToDo(toDo) {
+        const item = `
         <li class="todo-item">
             <p class="text">${toDo}</p>
             <img class="todo-delete" src="res/img/close.jpg" alt="close"/>
         </li>
-    `;
+        `;
+        list.insertAdjacentHTML("afterbegin", item);
+    }
 
-    list.insertAdjacentHTML("afterbegin", item);
-}
-
-function onAddNewTodo(evt) {
-    if (evt.key === "Enter") {
-        let toDo = input.value;
-        if (toDo) {
-            addToDo(toDo);
+    function AddNewTodo(evt) {
+        if (evt.key === "Enter") {
+            let toDo = input.value;
+            if (toDo) {
+                addToDo(toDo);
+            }
+            input.value = "";
         }
-        input.value = "";
     }
-}
 
-function toggleTodo(evt) {
-    const element = evt.target.closest(".todo-item");
-    if (element) {
-        element.classList.toggle("complete");
+    function toggleTodo(evt) {
+        const element = evt.target.closest(".todo-item");
+        if (element) {
+            element.classList.toggle("complete");
+        }
     }
-}
 
-function removeTodo(evt){
-    const todoRemoveEl = evt.target.closest(".todo-delete");
-    if(todoRemoveEl){
-        const todoEl = evt.target.closest(".todo-item");
-        todoEl.remove();
+    function removeTodo(evt) {
+        const todoRemoveEl = evt.target.closest(".todo-delete");
+        if (todoRemoveEl) {
+            const todoEl = evt.target.closest(".todo-item");
+            todoEl.remove();
+        }
     }
+
+    container.addEventListener("keyup", AddNewTodo);
+
+    container.addEventListener("click", toggleTodo);
+
+    container.addEventListener("click", removeTodo);
 }
+const todoInsertPoint1El = document.querySelector("#todoInsertPoint1");
+const todoInsertPoint2El = document.querySelector("#todoInsertPoint2");
 
-document.addEventListener("keyup", onAddNewTodo);
-
-document.addEventListener("click", toggleTodo);
-
-document.addEventListener("click", removeTodo);
+new ToDolist(todoInsertPoint1El,[
+    "todo1",
+    "todo2",
+    "todo3",
+]);
+new ToDolist(todoInsertPoint2El,[
+    "todo1",
+    "todo2",
+    "todo3",
+]);
